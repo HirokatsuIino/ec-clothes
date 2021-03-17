0d71edaac401825c22cc2b0ea1e06dce81772c88
hirokatsuiino <letterbox6212>
2021-03-17 18:24:22 +0900

    [ADD]style

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/hirokatsuiino/IdeaProjects/ec-clothes-workspace/ec-clothes/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/hirokatsuiino/IdeaProjects/ec-clothes-workspace/ec-clothes/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
