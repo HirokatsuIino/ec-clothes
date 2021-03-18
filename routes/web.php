7e6953c46b5fd45c714d0d704fac93bf72017f9f
hirokatsuiino <letterbox6212>
2021-03-18 15:58:25 +0900

    [ADD]cart root

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,17 @@
 |
 */
 
-Route::get('/products', 'ProductController@index')->name('product.index');
-Route::get('/product/{id}', 'ProductController@show')->name('product.show');
+//Route::get('/products', 'ProductController@index')->name('product.index');
+//Route::get('/product/{id}', 'ProductController@show')->name('product.show');
 
-//Route::name('product.')
-//    ->group(function () {
-//        Route::get('/', 'ProductController@index')->name('index');
-//        Route::get('/product/{id}', 'ProductController@show')->name('show');
-//    });
+Route::name('product.')
+    ->group(function () {
+        Route::get('/', 'ProductController@index')->name('index');
+        Route::get('/product/{id}', 'ProductController@show')->name('show');
+    });
+
+Route::name('line_item.')
+    ->group(function () {
+        Route::post('/line_item/create', 'LineItemController@create')->name('create');
+    });
+
