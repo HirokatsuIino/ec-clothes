141a9f5fd7a0d98f4159a716b17679f35746521a
hirokatsuiino <letterbox6212>
2021-03-18 15:36:10 +0900

    [ADD]リレーション

--- app/Cart.php
+++ app/Cart.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     //
+    public function products()
+    {
+        return $this->belongsToMany(
+            Product::class,
+            'line_items',
+         )->withPivot(['id', 'quantity']);
+    }
 }
