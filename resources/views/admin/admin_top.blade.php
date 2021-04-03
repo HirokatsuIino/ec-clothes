00c18a769d0d3e250d9291cfb5f77b43674ed87f
hirokatsuiino <letterbox6212>
2021-04-03 15:44:00 +0900

    [ADD]管理者一覧への遷移ボタン

--- resources/views/admin/admin_top.blade.php
+++ resources/views/admin/admin_top.blade.php
@@ -7,6 +7,7 @@
         <div class="card-body">
             <div>
                 <a href="{{ url('admin/user_list') }}" class="btn btn-primary">ユーザー一覧</a>
+                <a href="{{ url('admin/admin_list') }}" class="btn btn-primary">管理者一覧</a>
             </div>
         </div>
     </div>
